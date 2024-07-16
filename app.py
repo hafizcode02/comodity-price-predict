6a23b7d1c6a76070386fa4c4bde2ef8a9bb7fa52
Hafiz Caniago <hafizcode02@gmail.com>
2024-07-16 12:23:46 +0700

    add api function to get data from xlsx file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from src.predict.index import predict
 from src.web.index import web
+from src.comodity.index import comodity
 from flask import Flask, request, jsonify
 
 # Initialize Flask App
@@ -8,6 +9,13 @@ app = Flask(__name__)
 
 app.register_blueprint(web)
 app.register_blueprint(predict)
+app.register_blueprint(comodity)
+
+# Print all routes
+def print_routes():
+    for rule in app.url_map.iter_rules():
+        print(f"Endpoint: {rule.endpoint}, URL: {rule}")
 
 if __name__ == "__main__":
+    print_routes()  # Call function to print routes
     app.run(debug=True, host="localhost" , port=2024)
---
+++ src/comodity/index.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, request, jsonify
+import pandas as pd
+
+comodity = Blueprint('comodity', __name__)
+
+# Load the Excel file
+df = pd.read_excel('storage/comodity-price.xlsx', parse_dates=['date'])
+# Convert the 'date' to Unix timestamp in milliseconds
+df['unix_timestamp'] = df['date'].apply(lambda x: int(x.timestamp() * 1000))
+
+@comodity.route('/data-cabai-merah-besar')
+def data_cabai_merah_besar():
+    # Select the required columns and convert to a list of lists
+    data_list = df[['unix_timestamp', 'cabai_merah_besar']].values.tolist()
+    
+    # Return JSON response
+    return jsonify(data=data_list)
+
+@comodity.route('/data-cabai-merah-keriting')
+def data_cabai_merah_keriting():
+    # Select the required columns and convert to a list of lists
+    data_list = df[['unix_timestamp', 'cabai_merah_keriting']].values.tolist()
+    
+    # Return JSON response
+    return jsonify(data=data_list)
+
+@comodity.route('/data-cabai-rawit-hijau')
+def data_cabai_rawit_hijau():
+    # Select the required columns and convert to a list of lists
+    data_list = df[['unix_timestamp', 'cabai_rawit_hijau']].values.tolist()
+    
+    # Return JSON response
+    return jsonify(data=data_list)
+
+@comodity.route('/data-cabai-rawit-merah')
+def data_cabai_rawit_merah():
+    # Select the required columns and convert to a list of lists
+    data_list = df[['unix_timestamp', 'cabai_rawit_merah']].values.tolist()
+    
+    # Return JSON response
+    return jsonify(data=data_list)
+
+@comodity.route('/data-bawang-merah')
+def data_bawang_merah():
+    # Select the required columns and convert to a list of lists
+    data_list = df[['unix_timestamp', 'bawang_merah']].values.tolist()
+    
+    # Return JSON response
+    return jsonify(data=data_list)
