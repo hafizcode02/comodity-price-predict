898495b96c680265494c743e0710930f5106f4fb
Hafiz Caniago <hafizcode02@gmail.com>
2024-07-25 16:24:06 +0700

    update request body & customize days_prediction, option to use raw or data from xlsx

--- src/predict/index.py
+++ src/predict/index.py
@@ -1,9 +1,23 @@
+# Define the API endpoint for making predictions, predictions for the next x days, 
+# where x is the number of steps in the future we want to predict.
+# The endpoint expects a POST request with a JSON payload containing the last 30 observations.
+# The endpoint returns a JSON response containing the predicted prices for the next x days.
+# The endpoint uses the pre-trained model and scaler to make predictions.
+# The correct data input is only used on the first day prediction, on the second day and next the first day prediction and next (output) will be used as new data to be used for prediction.
+# example : 0-29 days data input, predict for day 30. day 30 prediction will be used as new data input for day 31 prediction, and so on.
+
 from flask import Blueprint, request, jsonify
 from tensorflow.keras.models import load_model # type: ignore
 import numpy as np
 import joblib
+import pandas as pd
+import os
 
+# Create a new Blueprint for the predict API
 predict = Blueprint('predict', __name__)
+
+# Define the path to the dataset
+dataset_path = 'storage/dataset.xlsx'
 
 # Load the pre-trained model and scaler
 model_bawang_merah = load_model('storage/model_bawang_merah.h5')
@@ -21,36 +35,62 @@ scaler_cabai_rawit_hijau = joblib.load('storage/scaler_bawang_merah.pkl')
 model_cabai_rawit_merah = load_model('storage/model_bawang_merah.h5')
 scaler_cabai_rawit_merah = joblib.load('storage/scaler_bawang_merah.pkl')
 
-# Define the API endpoint for making predictions, predictions for the next x days, 
-# where x is the number of steps in the future we want to predict.
-# The endpoint expects a POST request with a JSON payload containing the last 30 observations.
-# The endpoint returns a JSON response containing the predicted prices for the next x days.
-# The endpoint uses the pre-trained model and scaler to make predictions.
-# The correct data input is only used on the first day prediction, on the second day and next the first day prediction and next (output) will be used as new data to be used for prediction.
-# example : 0-29 days data input, predict for day 30. day 30 prediction will be used as new data input for day 31 prediction, and so on.
-
+# Base Route
 @predict.route('/api')
 def index():
     return "API - Comodity Price Predict"
 
+# Validate Request JSON
+def validate_request_data(required_fields):
+    data = request.get_json(force=True)
+    missing_fields = [field for field in required_fields if field not in data or data[field] is None]
+
+    if missing_fields:
+        return jsonify({"error": f"Missing or null fields: {', '.join(missing_fields)}"}), 400
+
+    return data
+
+# Predict API
 @predict.route('/api/predict-bawang-merah', methods=['POST'])
 def predict_bawang_merah():
-    data = request.get_json(force=True)
+    # Required Fields
+    required_fields = ['days_prediction', 'use_raw_data', 'last_observations']
+    validation_result = validate_request_data(required_fields)
+    
+    if isinstance(validation_result, tuple):  # If it's a tuple, it's an error response
+        return validation_result
+    
+    # Fetch the data from the validation result
+    data = validation_result
+    days_prediction = data['days_prediction']
+    use_raw_data = data['use_raw_data']
     last_observations = data['last_observations']
 
     # Ensure we have the right number of observations
     window_size = 30
-    if len(last_observations) != window_size:
-        return jsonify({'error': 'Provide exactly {} last observations'.format(window_size)}), 400
-
-    # Scale the input data
-    scaled_data = scaler_bawang_merah.transform(np.array(last_observations).reshape(-1, 1))
+    
+    # Check if use the raw data
+    if use_raw_data == True:
+        if len(last_observations) != window_size:
+            return jsonify({'error': 'Provide exactly {} last observations'.format(window_size)}), 400
+        
+        # Scale the input data from last observations
+        scaled_data = scaler_bawang_merah.transform(np.array(last_observations).reshape(-1, 1))
+    else:
+        # Load the dataset
+        if not os.path.exists(dataset_path):
+            return jsonify({'error': 'Dataset not found, upload the .xlsx file first'}), 404
+        
+        df = pd.read_excel(dataset_path)
+        df_sorted = df.sort_values(by='date')
+        df_sorted_tail = df_sorted.tail(window_size)
+        scaled_data = scaler_bawang_merah.transform(df_sorted_tail['bawang_merah'].values.reshape(-1, 1))
 
     # Prepare the input data for prediction
     future_data = scaled_data[-window_size:, :] # 30 last observations
 
     # Predict the next days
-    future_steps = 1
+    future_steps = days_prediction
     future_predictions = []
     for _ in range(future_steps):
         # reshape the input data (30, 1) to (1, 30, 1) 
